c745ad7cd4b7c4ee98cf0ae72ae590098d8a7b4d
Subhash Birajdar <subhashchandrabirajdar98@gmail.com>
2025-12-15 12:42:13 +0530

    first commit

---
+++ src/main/java/com/example/rashmika/RashmikaApplication.java
@@ -0,0 +1,13 @@
+package com.example.rashmika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RashmikaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RashmikaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rashmika/RashmikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rashmika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RashmikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
